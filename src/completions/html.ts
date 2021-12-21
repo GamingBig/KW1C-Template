f4530531acf1fa3c81633d023355a097b8585fa8
Joery <joery@joery.com>
2021-12-21 22:33:10 +0100

    see changelog for v0.0.3

--- src/completions/html.ts
+++ src/completions/html.ts
@@ -1,9 +1,15 @@
-import { ExtensionContext, languages, TextDocument, Position, CancellationToken, CompletionContext, workspace, CompletionItem, SnippetString, MarkdownString } from "vscode";
-import * as extraHTMLFile from "../extra Options/extraHTML.json";
+import { ExtensionContext, languages, TextDocument, Position, CancellationToken, CompletionContext, workspace, CompletionItem, SnippetString, MarkdownString, WorkspaceConfiguration, ConfigurationChangeEvent } from "vscode";
+import * as extraHTMLFile from "../extra Options/HTMLOptions.json";
 
-export function htmlCompletion(context: ExtensionContext){
+export function htmlCompletion(context: ExtensionContext, config: WorkspaceConfiguration){
     
     var extraHTML = JSON.parse(JSON.stringify(extraHTMLFile))
+
+	// reload config if it changes
+	workspace.onDidChangeConfiguration((event: ConfigurationChangeEvent) => {
+		config = workspace.getConfiguration("kw1c-template")
+	})
+
 
     return languages.registerCompletionItemProvider('html', {
 		
@@ -15,14 +21,20 @@ export function htmlCompletion(context: ExtensionContext){
             
             // Get trigger from settings
             var configuration = workspace.getConfiguration("kw1c-template");
-            var trigger = configuration.triggerWord
+            var trigger = config.triggerWord
             
-			var extraHtmlConfig = configuration.htmlExtras
+			var extraHtmlConfig = config.htmlExtras
+			var addTime = config.addTime
 			var htmlSnippet = extraHTML[extraHtmlConfig].join("\n")
 			
 			// Get the users' name from settings.
-			let userName = configuration.Name
+			let userName = config.Name
+			// Modify snippet
 			htmlSnippet = htmlSnippet.replace("{userName}", userName)
+				// Dont add time
+			if (!addTime) {
+				htmlSnippet = htmlSnippet.replace(" $CURRENT_HOUR:$CURRENT_MINUTE", "")
+			}
 
 			const htmlCompletion = new CompletionItem(trigger);
 			htmlCompletion.insertText = new SnippetString(
